31d9e9e6f2c9a9436d0deefae98a1e3b18d015e7
Alexandr_Li <muzikant1990@gmail.com>
2016-06-02 16:58:15 +0600

    Add getters/setters

--- src/main/java/com/epam/ali/spring/model/ModelT1000.java
+++ src/main/java/com/epam/ali/spring/model/ModelT1000.java
@@ -14,6 +14,30 @@ public class ModelT1000 implements Robot {
         this.leg = leg;
     }
 
+    public Head getHead() {
+        return head;
+    }
+
+    public void setHead(Head head) {
+        this.head = head;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+
+    public void setHand(Hand hand) {
+        this.hand = hand;
+    }
+
+    public Leg getLeg() {
+        return leg;
+    }
+
+    public void setLeg(Leg leg) {
+        this.leg = leg;
+    }
+
     @Override
     public void action() {
         head.calc();
