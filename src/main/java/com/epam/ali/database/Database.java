238d7461e8011600e9bece8a1e758a035eb2a506
Alexandr.li <muzikant1990@gmail.com>
2016-06-14 14:14:10 +0600

    Add Database class

---
+++ src/main/java/com/epam/ali/database/Database.java
@@ -0,0 +1,12 @@
+package com.epam.ali.database;
+
+public class Database {
+
+    public Database() {
+        testConnection();
+    }
+
+    private void testConnection() {
+        System.out.println("Database connection initialized");
+    }
+}
