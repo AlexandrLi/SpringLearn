2f9032132ddaf004e0e35a30abdd8a49eec99983
Alexandr.li <muzikant1990@gmail.com>
2016-06-15 15:34:56 +0600

    Test RobotPool realization

--- src/test/java/com/epam/ali/SpringLearnApplicationTests.java
+++ src/test/java/com/epam/ali/SpringLearnApplicationTests.java
@@ -1,7 +1,7 @@
 package com.epam.ali;
 
-import com.epam.ali.spring.model.Robot;
-import com.epam.ali.spring.model.T1000Conveyor;
+import com.epam.ali.spring.SpringLearnApplication;
+import com.epam.ali.spring.model.T1000Pool;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -15,13 +15,15 @@ public class SpringLearnApplicationTests {
     @Test
     public void contextLoads() {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("App-context.xml");
-        T1000Conveyor t1000Conveyor = context.getBean("t1000Conveyor", T1000Conveyor.class);
-        Robot robot1 = t1000Conveyor.createRobot();
-        robot1.action();
-        Robot robot2 = t1000Conveyor.createRobot();
-        robot2.action();
-//        ModelT1000 t1000 = context.getBean("t1000", ModelT1000.class);
-//        t1000.action();
+//        T1000Conveyor t1000Conveyor = context.getBean("t1000Conveyor", T1000Conveyor.class);
+//        Robot robot1 = t1000Conveyor.createRobot();
+//        robot1.action();
+//        Robot robot2 = t1000Conveyor.createRobot();
+//        robot2.action();
+        T1000Pool t1000Pool = context.getBean("t1000Pool", T1000Pool.class);
+        t1000Pool.action();
+        T1000Pool t1000FilteredPool = context.getBean("t1000FilteredPool", T1000Pool.class);
+        t1000FilteredPool.action();
     }
 
 }
