273e033ef0250982946f85f8c4c8386988770a59
Alexandr.li <muzikant1990@gmail.com>
2016-06-15 15:34:33 +0600

    Optimize imports

--- src/main/java/com/epam/ali/spring/model/RobotConveyor.java
+++ src/main/java/com/epam/ali/spring/model/RobotConveyor.java
@@ -1,5 +1,7 @@
 package com.epam.ali.spring.model;
 
+import com.epam.ali.spring.model.robot.Robot;
+
 public interface RobotConveyor {
     Robot createRobot();
 }
--- src/main/java/com/epam/ali/spring/model/T1000Conveyor.java
+++ src/main/java/com/epam/ali/spring/model/T1000Conveyor.java
@@ -1,5 +1,7 @@
 package com.epam.ali.spring.model;
 
+import com.epam.ali.spring.model.robot.Robot;
+
 public abstract class T1000Conveyor implements RobotConveyor {
 
     @Override
