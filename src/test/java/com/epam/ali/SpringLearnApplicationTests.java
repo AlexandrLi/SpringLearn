4051137cbd9efc808229a4a5d2383234d7f0a995
Alexandr.li <muzikant1990@gmail.com>
2016-06-16 15:29:12 +0600

    Clean up code

--- src/test/java/com/epam/ali/SpringLearnApplicationTests.java
+++ src/test/java/com/epam/ali/SpringLearnApplicationTests.java
@@ -15,11 +15,6 @@ public class SpringLearnApplicationTests {
     @Test
     public void contextLoads() {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("App-context.xml");
-//        T1000Conveyor t1000Conveyor = context.getBean("t1000Conveyor", T1000Conveyor.class);
-//        Robot robot1 = t1000Conveyor.createRobot();
-//        robot1.action();
-//        Robot robot2 = t1000Conveyor.createRobot();
-//        robot2.action();
         T1000Pool t1000Pool = context.getBean("t1000Pool", T1000Pool.class);
         t1000Pool.action();
         T1000Pool t1000FilteredPool = context.getBean("t1000FilteredPool", T1000Pool.class);
