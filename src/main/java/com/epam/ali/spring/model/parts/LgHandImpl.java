c36b9673a732299a1d83e7572797f7fe8302f5c6
Alexandr.li <muzikant1990@gmail.com>
2016-06-16 15:25:45 +0600

    Add new implementations of robot parts

---
+++ src/main/java/com/epam/ali/spring/model/parts/LgHandImpl.java
@@ -0,0 +1,10 @@
+package com.epam.ali.spring.model.parts;
+
+public class LgHandImpl implements Hand {
+
+    @Override
+    public void catchSomething() {
+        System.out.println("Catching LG G5");
+    }
+
+}
---
+++ src/main/java/com/epam/ali/spring/model/parts/LgHeadImpl.java
@@ -0,0 +1,8 @@
+package com.epam.ali.spring.model.parts;
+
+public class LgHeadImpl implements Head {
+    @Override
+    public void calc() {
+        System.out.println("LG calculating...");
+    }
+}
