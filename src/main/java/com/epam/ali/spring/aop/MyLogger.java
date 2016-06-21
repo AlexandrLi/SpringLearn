b371046f8c584eec07296a21ccfc880bb33ddd96
Alexandr.li <muzikant1990@gmail.com>
2016-06-21 16:23:01 +0600

    Add watch timer for all methods through MyLogger aspect

--- src/main/java/com/epam/ali/spring/aop/MyLogger.java
+++ src/main/java/com/epam/ali/spring/aop/MyLogger.java
@@ -1,6 +1,10 @@
 package com.epam.ali.spring.aop;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 import javax.inject.Named;
+import java.util.Map;
+import java.util.Set;
 
 @Named
 public class MyLogger {
@@ -15,5 +19,27 @@ public class MyLogger {
 
     public void close() {
         System.out.println("close");
+    }
+
+    public Object watchTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("Begin method " + joinPoint.getSignature().toShortString());
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            System.out.println("Path: " + arg);
+        }
+        Object output = joinPoint.proceed();
+        print(output);
+        long time = System.currentTimeMillis() - start;
+        System.out.println("End method " + joinPoint.getSignature().toShortString() + " time = " + time + " ms");
+        return output;
+    }
+
+    private void print(Object collection) {
+        if (collection instanceof Set) {
+            ((Set) collection).forEach(System.out::println);
+        } else if (collection instanceof Map) {
+            ((Map) collection).forEach((o, o2) -> System.out.println("key: " + o + " value: " + o2));
+        }
     }
 }
--- src/test/java/com/epam/ali/SpringLearnApplicationTests.java
+++ src/test/java/com/epam/ali/SpringLearnApplicationTests.java
@@ -1,5 +1,6 @@
 package com.epam.ali;
 
+import com.epam.ali.spring.aop.FileManager;
 import com.epam.ali.spring.aop.SomeService;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -10,8 +11,11 @@ public class SpringLearnApplicationTests {
     public void contextLoads() {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("App-context.xml");
         SomeService someService = context.getBean("someService", SomeService.class);
+        FileManager fileManager = context.getBean("fileManager", FileManager.class);
         someService.getDoubleValue();
         someService.getIntValue();
+        fileManager.getExtensionList("C:\\Windows\\System32");
+        fileManager.getExtensionCount("C:\\Windows\\System32");
 //        T1000Pool t1000Pool = context.getBean("t1000Pool", T1000Pool.class);
 //        t1000Pool.action();
 //        T1000Pool t1000FilteredPool = context.getBean("t1000FilteredPool", T1000Pool.class);
