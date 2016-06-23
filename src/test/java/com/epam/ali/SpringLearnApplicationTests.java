7330f26272d2fc0ff956a6d50ab39aac36e7db87
Alexandr.li <muzikant1990@gmail.com>
2016-06-23 12:39:49 +0600

    remove unused code

--- src/test/java/com/epam/ali/SpringLearnApplicationTests.java
+++ src/test/java/com/epam/ali/SpringLearnApplicationTests.java
@@ -16,10 +16,6 @@ public class SpringLearnApplicationTests {
         someService.getIntValue();
         fileManager.getExtensionList("C:\\Windows\\System32");
         fileManager.getExtensionCount("C:\\Windows\\System32");
-//        T1000Pool t1000Pool = context.getBean("t1000Pool", T1000Pool.class);
-//        t1000Pool.action();
-//        T1000Pool t1000FilteredPool = context.getBean("t1000FilteredPool", T1000Pool.class);
-//        t1000FilteredPool.action();
     }
 
 }
